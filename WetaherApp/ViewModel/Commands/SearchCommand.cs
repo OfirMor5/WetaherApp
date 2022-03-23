c7d88ed78c2b57a9af1408ab7bd8646c6c88eef3
OfirMor <ofrimor@gmail.com>
2022-03-23 13:36:59 +0200

    search and show the temp of specific city

---
+++ WetaherApp/ViewModel/Commands/SearchCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherApp.ViewModel;
+
+namespace WetaherApp.ViewModel.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public WeatherVM VM { get; set; }
+        public SearchCommand(WeatherVM weatherVM)
+        {
+            VM = weatherVM;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            VM.MakeQuery();
+        }
+    }
+}
--- WetaherApp/ViewModel/WeatherVM.cs
+++ WetaherApp/ViewModel/WeatherVM.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WeatherApp.Model;
+using WeatherApp.ViewModel.Helpers;
+using WetaherApp.ViewModel.Commands;
 
 namespace WeatherApp.ViewModel
 {
     public class WeatherVM : INotifyPropertyChanged
     {
+        public SearchCommand SearchCommand { get; set; }
+        public ObservableCollection<City> Cities { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private CurrentConditions currentConditions;
         private string query;
+        private City selectedCity;
+
 
         public string Query
         {
@@ -22,19 +32,16 @@ namespace WeatherApp.ViewModel
             }
         }
 
-        private CurrentConditions currentConditions;
 
         public CurrentConditions CurrentConditions
         {
             get => currentConditions;
             set
             {
-                CurrentConditions = value;
+                currentConditions = value;
                 OnPropertyChanged("currentConditions");
             }
         }
-
-        private City selectedCity;
 
         public City SelectedCity
         {
@@ -43,15 +50,55 @@ namespace WeatherApp.ViewModel
             {
                 selectedCity = value;
                 OnPropertyChanged("SelectedCity");
+                GetCurrentConditions();
             }
         }
 
+
+
         public WeatherVM()
         {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                selectedCity = new City
+                {
+                    LocalizedName = "jerusalem"
+                };
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "sunny",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = "7"
+                        }
+                    }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
 
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private async void GetCurrentConditions()
+        {
+            Query = string.Empty;
+            Cities.Clear();
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(selectedCity.Key);
+        }
+
+        public async Task MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            foreach(var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
 
         private void OnPropertyChanged(string propertyName)
         {
